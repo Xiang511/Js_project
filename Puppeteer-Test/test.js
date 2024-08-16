9121ea6ff59bc8bd92ad2ae818f16d1a384b6ce1
Xiang511 <toby91511@gmail.com>
2024-08-16 15:10:55 +0800

    feat: Puppeteer 爬取 Threads

--- Puppeteer-Test/test.js
+++ Puppeteer-Test/test.js
@@ -1,27 +1,85 @@
-const puppeteer = require('puppeteer-core');
+document.getElementById('fileInput').addEventListener('change', function () {
+    var fileInput = document.getElementById('fileInput');
+    if (fileInput.files.length > 0) {
+        var file = fileInput.files[0];
+        var reader = new FileReader();
 
-async function getProfilePicture(username) {
-  const browser = await puppeteer.launch({
-    executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', // 指定 Chrome 瀏覽器的路徑
-    headless: false
-  });
-  const page = await browser.newPage();
-  await page.goto(`https://www.instagram.com/${username}`, { waitUntil: 'networkidle2' });
+        reader.onload = function (e) {
+            var content = e.target.result;
+            try {
+                var json = JSON.parse(content);
+                followersData = json;
 
-  // 等待頭貼圖片載入
-  await page.waitForSelector('img[alt*="的大頭貼照"]', { timeout: 30000 });
+                
 
-  const profilePictureElement = await page.$('img[alt*="的大頭貼照"]');
-  const profilePictureUrl = await profilePictureElement.getProperty('src');
-  const profilePictureUrlValue = await profilePictureUrl.jsonValue();
+            } catch (error) {
+                console.error('解析 JSON 失敗:', error);
+                alert('檔案內容不是有效的 JSON 格式，請選擇正確的檔案');
+                fileInput.value = ''; // 清空檔案輸入欄位
+            }
+        };
 
-  await browser.close();
+        reader.readAsText(file); // 讀取檔案內容作為文字
+    } else {
+        alert('請選擇一個檔案');
+    }
+});
 
-  return profilePictureUrlValue;
+function main(){
+    var listElement = document.getElementById('followingList');
+        if (!listElement) {
+            listElement = document.createElement('ul');
+            listElement.id = 'followingList';
+            document.body.appendChild(listElement);
+        }
+
+        listElement.innerHTML = '';
+        console.log(followersData);
+        
+
+        followersData.forEach(item => {
+            var listItem = document.createElement('li');
+            var link = document.createElement('a');
+            var div = document.createElement('div');
+            var span = document.createElement('span');
+            //建立img元素
+            var img = document.createElement('img');
+
+            moment.locale('zh-tw');
+            if(YMDCheck.checked){
+                span.textContent = `您於 ${moment.unix(item.timestamp).format("YYYY年MM月")} 開始追蹤此用戶`;
+            }else{
+                span.textContent = `您於 ${moment.unix(item.timestamp).fromNow()} 開始追蹤此用戶`;
+            }
+            
+
+            span.style.fontSize = '.5em';
+            if (window.innerWidth > 992) {
+                span.style.fontSize = '1em';
+            }
+            listItem.style.display = 'flex';
+            listItem.style.justifyContent = 'space-between';
+            listItem.style.alignItems = 'center';
+            listItem.classList.add('list-group-item');
+         
+            link.textContent = item.value;
+            link.href = item.href;
+            link.target = "_blank";
+            img.style.width = '15%'
+            img.style.marginRight = '0.5em'
+            img.style.borderRadius = '50%';
+            img.src = item.profilePicture;
+            img.onerror = function() {
+                this.src = 'profile_pictures/default_profile_picture.png';
+
+            };
+        
+           
+            div.appendChild(img);
+            div.appendChild(link);
+            listItem.appendChild(div);      
+            listItem.appendChild(span);
+            result.appendChild(listItem);
+        });
 }
-
-// 使用示例
-getProfilePicture('dcard.tw')
-  .then(url => console.log(url))
-  .catch(error => console.error(error));
-  
+//將找不到的圖片預設為defale.png
